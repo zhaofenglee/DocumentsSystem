01f9e22c2f987b8104ba94c51c8423638071d1a8
zhaofenglee <zhaof_li@163.com>
2024-11-07 19:36:20 +0800

    Add docs module migrations

--- JS.DocumentsSystem/Migrations/20241107113352_Added_Docs_Module.Designer.cs
+++ JS.DocumentsSystem/Migrations/20241107113352_Added_Docs_Module.Designer.cs
@@ -4,6 +4,7 @@ using JS.DocumentsSystem.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace JS.DocumentsSystem.Migrations
 {
     [DbContext(typeof(DocumentsSystemDbContext))]
-    partial class DocumentsSystemDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241107113352_Added_Docs_Module")]
+    partial class Added_Docs_Module
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
